f3a0a812d0a3c1741a7da654e525ccd399e98f2b
huangjing <260741887@qq.com>
2019-01-14 15:47:04 +0800

    first commit

---
+++ template.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>test</title>
+<meta name="Keywords" content="" />
+<meta name="Description" content="" />
+<style type="text/css">
+*{list-style:none;font-size:14px;font-family:tahoma;}
+</style>
+</head>
+<body>
+
+
+<?php
+
+^!
+
+
+
+?>
+
+
+
+<script>
+
+
+
+</script>
+</body>
+</html>
+
